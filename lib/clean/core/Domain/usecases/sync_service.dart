fd403c283566fe1716c1da759ea938c1dec6be5a
Matvey <59060848+redduvel@users.noreply.github.com>
2025-01-07 05:08:15 +0600

    add check connection and singleton

--- lib/clean/core/Domain/usecases/sync_service.dart
+++ lib/clean/core/Domain/usecases/sync_service.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:io';
+import 'package:flutter/foundation.dart' as f;
 import 'package:bluetooth_app/clean/core/Domain/bloc/db.bloc.dart';
 import 'package:bluetooth_app/clean/core/Domain/entities/marking/category.dart';
 import 'package:bluetooth_app/clean/core/Domain/entities/marking/product.dart';
@@ -6,23 +8,62 @@ import 'package:bluetooth_app/clean/core/Domain/entities/marking/template.dart';
 import 'package:bluetooth_app/clean/core/Domain/entities/marking/user.dart';
 
 class SyncService {
-  Timer? _timer;
-  final DBBloc<User> userBloc;
-  final DBBloc<Product> productBloc;
-  final DBBloc<Category> categoryBloc;
-  final DBBloc<Template> templateBloc;
+  static final SyncService _instance = SyncService._internal();
+  factory SyncService() => _instance;
 
-  SyncService({
-    required this.userBloc,
-    required this.productBloc,
-    required this.categoryBloc,
-    required this.templateBloc,
-  });
+  SyncService._internal();
+
+  Timer? _timer;
+  late DBBloc<User> userBloc;
+  late DBBloc<Product> productBloc;
+  late DBBloc<Category> categoryBloc;
+  late DBBloc<Template> templateBloc;
+
+  static Future<SyncService> initialize({
+    required DBBloc<User> userBloc,
+    required DBBloc<Product> productBloc,
+    required DBBloc<Category> categoryBloc,
+    required DBBloc<Template> templateBloc,
+  }) async {
+    _instance.userBloc = userBloc;
+    _instance.productBloc = productBloc;
+    _instance.categoryBloc = categoryBloc;
+    _instance.templateBloc = templateBloc;
+    return _instance;
+  }
 
   void startSync() {
-    _timer = Timer.periodic(const Duration(minutes: 3), (timer) {
-      _syncAll();
+    _timer = Timer.periodic(const Duration(minutes: 3), (timer) async {
+      await _syncInBackground();
     });
+  }
+
+  Future<bool> _checkInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  Future<void> _syncInBackground() async {
+    try {
+      final hasInternet = await _checkInternetConnection();
+      if (!hasInternet) {
+        print('Нет подключения к интернету. Синхронизация отложена.');
+        return;
+      }
+      
+      await f.compute(_backgroundSync, null);
+      _syncAll();
+    } catch (e) {
+      print('Ошибка при синхронизации: $e');
+    }
+  }
+
+  static Future<void> _backgroundSync(_) async {
+    await Future.delayed(const Duration(seconds: 1));
   }
 
   void stopSync() {
